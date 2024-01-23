5a85a36b3839fca6bb8414520efc1dab5ff32d16
Sofía Martínez Bauer <martinezbauersofia@gmail.com>
2024-01-23 20:05:42 -0300

    NOOOOO

---
+++ semana8.py/agenda.py
@@ -0,0 +1,7 @@
+# Agenda de contactos
+# Crear un menu con las opciones de agendar contacto y ver informacion de
+# contacto. Para agendar se solicitara al usuario un: nombre, apellido,
+# telefono, direccion. Para ver informacion se pedira el nombre y apellido.
+# La informacion sera un listado de diccionarios, donde cada diccionario
+# tendra como claves lo solicitado al usuario y como valor lo que ingrese el
+# usuario. A su vez, este listado debe estar guardado en un archivo json.
